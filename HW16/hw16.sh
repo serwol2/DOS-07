aef5de35a64d784f23ad80138b13cdf11af4d81c
serwol <adamal_serg@tut.by>
2021-12-08 19:24:12 +0300

    continue HW16-1
    continue WH16-2

---
+++ HW16/hw16.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+cat /proc/versio
+
+echo -n $(lsb_release -is)" "
+echo -n $(lsb_release -cs)" "
+echo $(uname -r)
+echo $(hostname -I)
+echo 'Load Average (15 min):' $(uptime |awk '{print $10;}') $(uptime -p)
+echo 'Used memory:' $(free -h| grep "Mem" | awk '{print $3;}')
+echo 'Users:' $(who -q)
+echo
